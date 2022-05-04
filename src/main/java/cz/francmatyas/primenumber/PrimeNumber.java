65d0b89c81e94b2ee84fc72173430cc042d52029
Matyas Franc <franc.matyas@gmail.com>
2022-05-04 23:15:27 +0200

    path input, number testing

--- src/main/java/cz/francmatyas/primenumber/PrimeNumber.java
+++ src/main/java/cz/francmatyas/primenumber/PrimeNumber.java
@@ -3,54 +3,69 @@ package cz.francmatyas.primenumber;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.Scanner;
 
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class PrimeNumber {
     public static void main(String[] args) throws IOException
     {
+        //System.out.println(Runtime.getRuntime().availableProcessors());
+        System.out.println("Paste the full file path into .xlsx document.");
+        System.out.flush();
+        Scanner scanner = new Scanner(System.in);
+
+        //String filePath = scanner.nextLine();
+
         String filePath = "C:\\Users\\franc\\Desktop\\sample data.xlsx";
+
         FileInputStream fileInputStream = new FileInputStream(filePath);
         XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
         XSSFSheet sheet = workbook.getSheetAt(0);
-        Iterator<Row> rowIterator = sheet.iterator();
 
-        while (rowIterator.hasNext())
-        {
-            Row row = rowIterator.next();
+        for (Row cells : sheet) {
+            XSSFRow row = (XSSFRow) cells;
             Iterator<Cell> cellIterator = row.cellIterator();
 
-            while (cellIterator.hasNext())
-            {
-                Cell cell = cellIterator.next();
-                try
-                {
-                    if(!isPrimeNumber(Integer.parseInt(cell.getStringCellValue())))
-                    {
-                        System.out.println(cell.getStringCellValue());
+            while (cellIterator.hasNext()) {
+                XSSFCell cell = (XSSFCell) cellIterator.next();
+                if (cell.getCellType() != CellType.BLANK) {
+                    try {
+                        double inputNumber = Double.parseDouble(cell.getStringCellValue());
+                        if (isInteger(inputNumber) && inputNumber > 0) {
+                            if (isPrimeNumber((int) inputNumber)) {
+                                System.out.println(inputNumber);
+                            }
+                        }
+                    } catch (Exception ignored) {
                     }
-                }
-                catch (Exception e)
-                {
                 }
             }
         }
     }
 
-    public static boolean isPrimeNumber(int number)
+    static boolean isInteger(double number)
     {
-        boolean result = false;
+        return number % 1 == 0;
+    }
+
+    static boolean isPrimeNumber(int number)
+    {
+        boolean result = true;
         if (number <= 1)
         {
-            result = true;
+            result = false;
         }
         else {
             for (int i = 2; i <= number / 2; i++) {
                 if (number % i == 0) {
-                    result = true;
+                    result = false;
                     break;
                 }
             }
