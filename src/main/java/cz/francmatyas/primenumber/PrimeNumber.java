5cd96d2c4eb216c9035bd0cbbf68f0f0a5ef07c0
Matyas Franc <franc.matyas@gmail.com>
2022-05-08 21:48:20 +0200

    multithreading

--- src/main/java/cz/francmatyas/primenumber/PrimeNumber.java
+++ src/main/java/cz/francmatyas/primenumber/PrimeNumber.java
@@ -3,14 +3,14 @@ package cz.francmatyas.primenumber;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.Objects;
 import java.util.Scanner;
 
+import org.apache.commons.io.FilenameUtils;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class PrimeNumber {
@@ -21,48 +21,60 @@ public class PrimeNumber {
         System.out.flush();
         Scanner scanner = new Scanner(System.in);
 
+
         //String filePath = scanner.nextLine();
 
         String filePath = "C:\\Users\\franc\\Desktop\\sample data.xlsx";
 
-        FileInputStream fileInputStream = new FileInputStream(filePath);
-        XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
-        XSSFSheet sheet = workbook.getSheetAt(0);
+        if(isExcelFile(filePath)) {
+            Sheet sheet = parseExcelFile(filePath);
 
-        for (Row cells : sheet) {
-            XSSFRow row = (XSSFRow) cells;
-            Iterator<Cell> cellIterator = row.cellIterator();
+            for (Row row : sheet) {
+                Iterator<Cell> cellIterator = row.cellIterator();
 
-            while (cellIterator.hasNext()) {
-                XSSFCell cell = (XSSFCell) cellIterator.next();
+                while (cellIterator.hasNext()) {
+                    Cell cell = cellIterator.next();
 
-                if (cell.getCellType() != CellType.BLANK) {
-                    new Thread(() -> {
-                        try {
-                            double nextNumber = Double.parseDouble(cell.getStringCellValue());
-                            if (isInteger(nextNumber) && nextNumber > 0) {
-                                if (isPrimeNumber((int) nextNumber)) {
-                                    System.out.println(nextNumber);
+                    if (cell.getCellType() != CellType.BLANK) {
+                        new Thread(() -> {
+                            try {
+                                double nextNumber = Double.parseDouble(cell.getStringCellValue());
+                                if (isInteger(nextNumber) && nextNumber > 0) {
+                                    if (isPrimeNumber((int) nextNumber)) {
+                                        System.out.println(nextNumber);
+                                    }
                                 }
+                            } catch (Exception ignored) {
                             }
-                        } catch (Exception ignored) {
-                        }
-                    }).start();
+                        }).start();
+                    }
                 }
             }
         }
+        else {
+            System.out.println("Invalid file type. Expected .xlsx (Entered: ."
+                    + FilenameUtils.getExtension(filePath) + ")");
+        }
     }
 
-    static boolean isInteger(double number)
-    {
+    static Sheet parseExcelFile(String filePath) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream(filePath);
+        XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
+
+        return workbook.getSheetAt(0);
+    }
+
+    static boolean isExcelFile(String filePath){
+        return Objects.equals(FilenameUtils.getExtension(filePath), "xlsx");
+    }
+
+    static boolean isInteger(double number) {
         return number % 1 == 0;
     }
 
-    static boolean isPrimeNumber(int number)
-    {
+    static boolean isPrimeNumber(int number) {
         boolean result = true;
-        if (number <= 1)
-        {
+        if (number == 0 || number == 1) {
             result = false;
         }
         else {
